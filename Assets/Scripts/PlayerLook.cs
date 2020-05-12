7d679c8606743dc4de614c8b549ab75728813707
Jackson <Jklagge1998@gmail.com>
2020-05-11 18:19:44 -0700

    added player look script.

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -6,34 +6,46 @@ public class PlayerLook : MonoBehaviour
 {
     // public variables
     public Transform Playerbody;
-    public float mouseSensitivity;
-    public Transform GunPosition;
-
+    public float mouseSensitivityX = 100f;
+    public float mouseSensitivityY = 50f;
+    public float xRotation;
+    public float yRotation;
+    public bool mouseLock;
+    public bool invertYaw;
+    public bool invertTheta;
     // private variables
-    private float xRotation = 0f;
-    private Vector3 originalGunPosition;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked; // locks player's mouse cursor to middle of screen.
-        originalGunPosition = GunPosition.position;
+        Cursor.lockState = CursorLockMode.Locked; // locks player's mouse cursor on screen.
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if condition ? true value  : false value
+        //if (mouseLock )
         // update camera rotation based on users mouse input
         // Note: Using Time.deltaTime to move player by time not frame rate
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityX * Time.deltaTime * (mouseLock ? 0 : 1);
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY * Time.deltaTime * (mouseLock ? 0 : 1);
 
-        xRotation -= mouseY;
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // looking up and down
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Limits player's camera up/down movement to 180 degrees 
-        GunPosition.localRotation = transform.localRotation;
-        
-        // rotate player
-        Playerbody.Rotate(Vector3.up * mouseX); // looking side to side
+        xRotation = xRotation + (invertTheta ? -mouseX : mouseX);
 
+        yRotation = yRotation + (invertYaw ? mouseY : -mouseY);
+
+        yRotation = Mathf.Clamp(yRotation, -89f, 89f); // Limits player's camera up/down movement to 180 degrees 
+
+        mouseX = xRotation * Mathf.Deg2Rad;
+        mouseY = yRotation * Mathf.Deg2Rad;
+
+        Vector3 forward = new Vector3(0f, Mathf.Sin(mouseY), Mathf.Cos(mouseY));
+
+        //Vector3 forward = new Vector3(Mathf.Cos(mouseX) * Mathf.Cos(mouseY), Mathf.Sin(mouseY), Mathf.Cos(mouseY) * Mathf.Sin(mouseX));
+        transform.localRotation = Quaternion.LookRotation(forward, Vector3.up);
+        forward = new Vector3(Mathf.Cos(mouseX), 0f, Mathf.Sin(mouseX));
+        Playerbody.localRotation = Quaternion.LookRotation(forward, Vector3.up);
     }
 }
