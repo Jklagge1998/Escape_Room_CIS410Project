101dcd30c449ab20ff3d0a1963ddffef6696e030
Hinkemeyers@gmail.com <hinkemeyers@gmail.com>
2020-05-26 13:41:38 -0700

    button puzzle implemented

---
+++ Assets/Scripts/BridgeButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeButton : MonoBehaviour
+{
+    public int currentHealth = 1;
+    public bool activated;
+    //public int box_count;
+    public Bridge bridge;
+    public List<BridgeButton> buttons;
+
+    void Start()
+    {
+        if (activated)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+    }
+
+    private void colorChange()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        activated = false;
+    }
+
+
+    public void Damage(int damageAmount)
+    {
+        if (!activated)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+            bridge.SendMessage("Move");
+            foreach (BridgeButton button in buttons)
+            {
+                button.SendMessage("colorChange");
+            }
+        }
+            
+
+    }
+}
