b6fa1b555a6c7c0b6529e6bde501ea5e25cad179
k27star <kstecker@uoregon.edu>
2020-06-03 15:45:56 -0700

    Countdown Timer Implemented Intro level

---
+++ Assets/Scripts/Countdown_Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Countdown_Timer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 40f;
+    public string sceneName;
+
+    [SerializeField] Text countdown_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+        countdown_text.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdown_text.text = currentTime.ToString("f2");
+
+        if (currentTime <= 0)
+        {
+            countdown_text.text = "0.00";
+            SceneManager.LoadScene(sceneName);
+        }
+        else if (currentTime <= 10)
+        {
+            countdown_text.color = Color.red;
+        }
+        else if (currentTime <= 20)
+        {
+            countdown_text.color = Color.yellow;
+        }
+    }
+}
